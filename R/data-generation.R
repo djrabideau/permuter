33c2e506a342c8ee4a5e40337497fc333147264b
djrabideau <djrabideau@g.harvard.edu>
2018-12-12 13:15:22 -0500

    gendata_crt: user-specified Sigma

--- R/data-generation.R
+++ R/data-generation.R
@@ -7,7 +7,9 @@
 #' The data will be sorted increasing by cluster then subject. The GLMM is
 #' \deqn{g(E[Y_{ki}]) = \mu + \alpha_k + \theta * X_k}
 #' where \eqn{X_k} is the treatment indicator for the \eqn{k}th cluster and
-#' \eqn{\alpha ~ N(0, \sigma^2)}.
+#' \eqn{\alpha ~ N(0, Sigma)}. If only \code{sigma} is specified, then
+#' \code{Sigma} is exchangeable with \code{sigma^2} on the diagonals and
+#' \code{rho * sigma^2} on the off-diagonals.
 #'
 #' @param family a description of the error distribution and link function to
 #'     be used in the data generation model. This can be a character string
@@ -19,7 +21,8 @@
 #' @param size vector of length 2 specifying range of cluster sizes that will be
 #'     drawn from a uniform distribution
 #' @param theta treatment effect
-#' @param sigma SD of random cluster effect
+#' @param sigma SD of random cluster effect; ignored if \code{Sigma} is specified
+#' @param Sigma optional user-specified covariance matrix for random cluster effects
 #' @param mu intercept in GLMM
 #' @param rho between-cluster correlation (assuming exchangeable covariance); if
 #'     non-zero, then there is corrrelation between clusters.
@@ -44,7 +47,7 @@
 #' # 6       1.6      1  6   1 0
 #' @export
 gendata_crt <- function(family = gaussian, nclus, size, theta = 0,
-                        sigma, mu, rho = 0, sd = 1) {
+                        sigma, Sigma, mu, rho = 0, sd = 1) {
   if (length(nclus) != 2)
     stop("length(nclus) should be 2")
   if (length(size) != 2)
@@ -55,8 +58,13 @@ gendata_crt <- function(family = gaussian, nclus, size, theta = 0,
   ntot <- sum(nis)
   mymu <- rep(mu, ntot)
   # covariance matrix for random cluster effects
-  Sigma <- diag(rep(sigma^2, nclus_tot))
-  Sigma[lower.tri(Sigma)] <- Sigma[upper.tri(Sigma)] <- sigma^2 * rho
+  if (missing(Sigma)) {
+    Sigma <- diag(rep(sigma^2, nclus_tot))
+    Sigma[lower.tri(Sigma)] <- Sigma[upper.tri(Sigma)] <- sigma^2 * rho
+  } else {
+    if (!identical(as.numeric(dim(Sigma)), c(nclus_tot, nclus_tot)))
+      stop(paste0("dim(Sigma) should be c(", nclus_tot, ', ', nclus_tot, ')'))
+  }
   # random cluster effects
   alpha <- MASS::mvrnorm(1, rep(0, nclus_tot), Sigma)
   myalpha <- rep(alpha, nis)
